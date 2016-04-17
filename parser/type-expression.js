8b6c406d0013b768f82199cbcbe1507081cff610
raynos <raynos2@gmail.com>
2016-04-16 19:00:20 -0700

    allow T[] as alias for Array<T>

--- parser/type-expression.js
+++ parser/type-expression.js
@@ -2,11 +2,25 @@
 
 var Parsimmon = require('parsimmon');
 
+var lexemes = require('./lexemes.js');
+var typeLiteral = require('./type-literal.js');
 var typeGeneric = require('./type-generic.js');
+var AST = require('../ast.js');
 var valueLiteral = require('./value-literal.js');
+
+var arrayType = Parsimmon.seq(
+    typeLiteral,
+    lexemes.openSquareBrace,
+    lexemes.closeSquareBrace
+).map(function buildArrayType(list) {
+    var type = list[0];
+
+    return AST.generic(AST.literal('Array'), [type]);
+});
 
 var typeOrValueLiteral = Parsimmon.alt(
     valueLiteral,
+    arrayType,
     typeGeneric
 );
 
